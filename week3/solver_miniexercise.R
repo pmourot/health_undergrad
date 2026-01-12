6077d6392354a228cbb29742898c9440605382cd
pmourot <pmourot@bu.edu>
2026-01-12 15:51:58 +0100

    Commit first part work.

---
+++ week3/solver_miniexercise.R
@@ -0,0 +1,11 @@
+#install.packages('nleqslv')
+library("nleqslv")
+
+uninsured = 0.5 * sqrt(20000) + 0.4 * sqrt(19000) + .1 * sqrt(10000)
+
+fnA <- function(x) {
+  y <- 0.5 * sqrt(20000-x) + 0.4 * sqrt(20000 - 500 - 0.2 * 500 - x) + 0.1 * sqrt(20000 - 500 - 9500*0.2 - x)-uninsured
+  return(y)
+}
+solA =nleqslv(c(0,10000), fnA)
+print(solA$x)
